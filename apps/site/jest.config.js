9ec47497cc28e2cdf503ad92ea9d4b7f4bc2d455
Javier Lopez <jalopez13@gmail.com>
2022-02-01 08:18:28 -0800

    Renamed nx-ci-demo-app to site.

--- apps/site/jest.config.js
+++ apps/site/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'nx-ci-demo-other',
+  displayName: 'site',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/nx-ci-demo-other',
+  coverageDirectory: '../../coverage/apps/site',
 };
--- apps/site/pages/_app.tsx
+++ apps/site/pages/_app.tsx
@@ -6,7 +6,7 @@ function CustomApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Welcome to nx-ci-demo-app!</title>
+        <title>Welcome to nx with next.js!</title>
       </Head>
       <main className="app">
         <Component {...pageProps} />
